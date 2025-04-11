2acb1db4029e42402db46659d6c493fef485a903
Aowuu <fpg.zachary@gmail.com>
2025-04-11 23:37:18 +0800

    2.configuration for basic config-nvim/lua/core/keymaps.lua

---
+++ lua/core/keymaps.lua
@@ -0,0 +1,16 @@
+vim.g.mapleader = " "  -- 设置<leader>为空格
+
+local map = vim.keymap.set  -- 快捷写法
+
+-- 基础操作
+map("n", "<leader>w", ":w<CR>", { desc = "保存" })
+map("n", "<leader>q", ":q<CR>", { desc = "退出" })
+map("n", "<leader>e", ":NvimTreeToggle<CR>", { desc = "文件树" })
+
+-- 移动优化
+map("n", "H", "^")  -- 行首
+map("n", "L", "$")  -- 行尾
+--map("n", "J", "5j") -- 向下5行
+--map("n", "K", "5k") -- 向上5行
+
+
