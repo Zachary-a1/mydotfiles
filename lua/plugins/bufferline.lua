45f3f4c38f5a690031fcd517d97442f876c7d990
Aowuu <fpg.zachary@gmail.com>
2025-04-16 18:47:29 +0800

    changed cmd "jumping to next/previous bufferline" to <A-j/k>

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -66,8 +66,8 @@ return {
 
     -- Keymaps
     local opts = { noremap = true, silent = true, desc = 'Go to Buffer' }
-    vim.keymap.set('n', '<leader>j', '<Cmd>BufferLineCycleNext<CR>', {})
-    vim.keymap.set('n', '<leader>k', '<Cmd>BufferLineCyclePrev<CR>', {})
+    vim.keymap.set('n', '<A-j>', '<Cmd>BufferLineCycleNext<CR>', {})
+    vim.keymap.set('n', '<A-k>', '<Cmd>BufferLineCyclePrev<CR>', {})
     vim.keymap.set('n', '<leader>c', ':Bdelete<CR>', {})
     vim.keymap.set('n', '<leader>1', "<cmd>lua require('bufferline').go_to_buffer(1)<CR>", opts)
     vim.keymap.set('n', '<leader>2', "<cmd>lua require('bufferline').go_to_buffer(2)<CR>", opts)
